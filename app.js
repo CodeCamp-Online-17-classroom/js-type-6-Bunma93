2d3682f30b882106bd7a35be2a7d2be84fbd055a
Bunma93 <bunma93@gmail.com>
2024-06-27 09:15:17 +0700

    Js-type6

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 // write code here
+console.log(typeof 'x');//string
+console.log(typeof x);//undefined
+console.log(typeof ``);//string
+console.log(typeof Infinity);//number
+console.log(typeof(100 / 0));//number
+console.log(typeof(1 + '3'));//string
+console.log(typeof(1 * 'a'));//ได้ค่า NaN เป็น number
+console.log(typeof null);//object
